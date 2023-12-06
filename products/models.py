ecf3c6e00d3c1af269610c068b433c11d1114c6b
mohammdhawa <ragab5434@gmail.com>
2023-12-07 00:14:39 +0300

    created the DB

--- products/models.py
+++ products/models.py
@@ -19,14 +19,14 @@ FLAG_TYPES = (
 )
 
 class Product(models.Model):
-    name = models.CharField(verbose_name=_('name'), max_length=120)
-    flag = models.CharField(verbose_name=_('flag'), max_length=50, choices=FLAG_TYPES)
-    price = models.FloatField(verbose_name=_('price'), )
-    image = models.ImageField(verbose_name=_('image'), upload_to='product')
-    sku = models.IntegerField(verbose_name=_('sku'), )
-    subtitle = models.TextField(verbose_name=_('subtitle'), max_length=500)
+    name = models.CharField(_('name'), max_length=120)
+    flag = models.CharField(_('flag'), max_length=50, choices=FLAG_TYPES)
+    price = models.FloatField(_('price'), )
+    image = models.ImageField(_('image'), upload_to='product')
+    sku = models.IntegerField(_('sku'), )
+    subtitle = models.TextField(_('subtitle'), max_length=500)
     description = models.TextField(verbose_name=_('description'), max_length=50000)
-    brand = models.ForeignKey('Brand', verbose_name=_('brand'), related_name='product_brand', on_delete=models.SET_NULL)
+    brand = models.ForeignKey('Brand', verbose_name=_('brand'), related_name='product_brand', on_delete=models.SET_NULL, null=True)
     tags =TaggableManager()
 
     slug = models.SlugField(blank=True, null=True)
@@ -43,13 +43,13 @@ class Product(models.Model):
 
 class ProductImages(models.Model):
     product = models.ForeignKey(Product, verbose_name=_('product'), related_name='product_image', on_delete=models.CASCADE)
-    image = models.ImageField(verbose_name=_('image'), upload_to='product_images')
+    image = models.ImageField(_('image'), upload_to='product_images')
 
 
 
 class Brand(models.Model):
-    name = models.CharField(verbose_name=_('name'), max_length=100)
-    image = models.ImageField(verbose_name=_('image'), upload_to='brand')
+    name = models.CharField(_('name'), max_length=100)
+    image = models.ImageField(_('image'), upload_to='brand')
 
     slug = models.SlugField(blank=True, null=True)
 
@@ -64,10 +64,10 @@ class Brand(models.Model):
 
 
 class Review(models.Model):
-    user = models.ForeignKey(User, verbose_name=_('user'), related_name='review_user', on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, verbose_name=_('user'), related_name='review_user', on_delete=models.SET_NULL, null=True)
     product = models.ForeignKey(Product, verbose_name=_('product'), related_name='review_roduct', on_delete=models.CASCADE)
-    review = models.TextField(verbose_name=_('review'), max_length=1000)
-    rate = models.IntegerChoices(verbose_name=_('rate'), choices=[(i, i) for i in range(1, 6)])
+    review = models.TextField(_('review'), max_length=1000)
+    rate = models.IntegerField(_('rate'), choices=[(i, i) for i in range(1, 6)])
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self) -> str:
