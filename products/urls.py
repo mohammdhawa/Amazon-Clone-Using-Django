be6eca149cc79bf2cae6cf4755ed3b40c4d37434
mohammdhawa <ragab5434@gmail.com>
2023-12-09 01:15:26 +0300

    create CBV for product list and for product detail

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from . import views
+
 
 urlpatterns = [
-    
+    path('', views.ProductList.as_view()),
+    path('<slug:slug>/', views.ProductDetail.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import (ListView, DetailView,
+                                  CreateView, UpdateView,
+                                  DeleteView)
+
+
+from .models import Product, Brand, Review
+
+
+class ProductList(ListView):
+    model = Product
+
+
+
+class ProductDetail(DetailView):
+    model = Product
