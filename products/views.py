e81f118fe0e6dd9799c675456324b63ffa1abf89
mohammdhawa <ragab5434@gmail.com>
2023-12-13 12:00:05 +0300

    add pagination in product list page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from typing import Any
+from django.db import models
 from django.shortcuts import render
 
 from django.views.generic import (ListView, DetailView,
@@ -11,6 +12,7 @@ from .models import (Product, Brand, Review, ProductImages)
 
 class ProductList(ListView):
     model = Product
+    paginate_by = 100
 
 
 
@@ -29,15 +31,28 @@ class ProductDetail(DetailView):
 
 class BrandList(ListView):
     model = Brand
+    paginate_by = 50
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        return context
+    
 
 
+class BrandDetail(ListView):
+    model = Product
+    template_name = 'products/brand_detail.html'
 
-class BrandDetail(DetailView):
-    model = Brand
+
+    def get_queryset(self):
+        brand = Brand.objects.get(slug=self.kwargs['slug'])
+        query_set = super().get_queryset().filter(brand=brand)
+        return query_set
+    
 
     def get_context_data(self, **kwargs: Any):
         context = super().get_context_data(**kwargs)
-        context['products'] = Product.objects.filter(brand=self.get_object())
-
+        context['brand'] = Brand.objects.get(slug=self.kwargs['slug'])
+        print(context)
         return context
     
