4643bb954c91d6378a087354e033e20f6714c9fb
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-07-22 09:22:37 +0300

    add prodct_list & product_details views)

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import (Product, ProductImages,
+                     Brand, Review)
+from django.views.generic import (ListView, DetailView)
+
 
 # Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+
+
+class ProductDetailView(DetailView):
+    model = Product
