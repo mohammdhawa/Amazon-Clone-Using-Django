9e86487d4eb195392006ebab74301829469948e3
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-07-22 18:09:08 +0300

    show product reviews by views

--- products/views.py
+++ products/views.py
@@ -12,3 +12,8 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = Review.objects.filter(product=self.get_object())
+        return context
