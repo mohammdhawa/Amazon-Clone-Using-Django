220cb8b5b484efd7ec2b8ac75052749d0e5ec094
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-07-22 18:46:06 +0300

    show realted products in product details file

--- products/views.py
+++ products/views.py
@@ -17,4 +17,5 @@ class ProductDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['reviews'] = Review.objects.filter(product=self.get_object())
         context['images'] = ProductImages.objects.filter(product=self.get_object())
+        context['related'] = Product.objects.filter(brand=self.get_object().brand)[:10]
         return context
