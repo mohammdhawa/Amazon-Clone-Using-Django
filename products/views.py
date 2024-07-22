c003553369d8e24ab2dc02629dff3c478be2a004
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-07-22 18:36:19 +0300

    show product images in product_detail file using views

--- products/views.py
+++ products/views.py
@@ -16,4 +16,5 @@ class ProductDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['reviews'] = Review.objects.filter(product=self.get_object())
+        context['images'] = ProductImages.objects.filter(product=self.get_object())
         return context
