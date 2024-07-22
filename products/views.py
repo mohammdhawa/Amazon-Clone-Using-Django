0089f03811af81bbe8612f7b3696f04dfa698825
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-07-22 21:51:15 +0300

    add brand detail using ListView logic

--- products/views.py
+++ products/views.py
@@ -25,10 +25,24 @@ class BrandListView(ListView):
     model = Brand
 
 
-class BrandDetailView(DetailView):
-    model = Brand
+# class BrandDetailView(DetailView):
+#     model = Brand
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['products'] = Product.objects.filter(brand=self.get_object())
+#         return context
+
+class BrandDetailView(ListView):
+    model = Product
+    template_name = 'products/brand_detail.html'
+
+    def get_queryset(self):
+        brand = Brand.objects.get(slug=self.kwargs['slug'])
+        queryset = super().get_queryset().filter(brand=brand)
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['products'] = Product.objects.filter(brand=self.get_object())
+        context['brand'] = Brand.objects.get(slug=self.kwargs['slug'])
         return context
