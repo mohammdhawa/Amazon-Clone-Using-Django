26ab5c06524971d0b3e0919b131d94fd88cc923d
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-07-23 09:54:41 +0300

    set global pagination for the api

--- AmazonClone/settings.py
+++ AmazonClone/settings.py
@@ -46,6 +46,12 @@ INSTALLED_APPS = [
     "products.apps.ProductsConfig",
 ]
 
+# Rest API
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
