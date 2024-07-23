703269fbaecf5af86b715b2cb8dd45a425c54dc4
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-07-23 12:41:27 +0300

    show avg_rate in product list & detail API

--- products/serializers.py
+++ products/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from .models import (Product, Brand)
 
@@ -6,6 +7,7 @@ class ProductListSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
     # count = serializers.SerializerMethodField(method_name='get_review_count') # if I want to name it 'count'
     review_count = serializers.SerializerMethodField()
+    avg_rate = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
@@ -14,10 +16,22 @@ class ProductListSerializer(serializers.ModelSerializer):
     def get_review_count(self, object):
         reviews = object.review_product.all().count()
         return reviews
+
+    def get_avg_rate(self, obj):
+        reviews = obj.review_product.all()
+
+        if not reviews:
+            return 0
+
+        total = sum(review.rate for review in reviews)
+        avg = round(total / len(reviews), 1)
+
+        return avg
 
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     review_count = serializers.SerializerMethodField()
+    avg_rate = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
@@ -26,6 +40,17 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     def get_review_count(self, object):
         reviews = object.review_product.all().count()
         return reviews
+
+    def get_avg_rate(self, obj):
+        reviews = obj.review_product.all()
+
+        if not reviews:
+            return 0
+
+        total = sum(review.rate for review in reviews)
+        avg = round(total / len(reviews), 1)
+
+        return avg
 
 
 class BrandListSerializer(serializers.ModelSerializer):
