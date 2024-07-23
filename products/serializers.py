ba569969c421f0275d96c717de2fd4d3a99ad2c5
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-07-23 11:00:02 +0300

    show review count in list & detail API

--- products/serializers.py
+++ products/serializers.py
@@ -4,16 +4,28 @@ from .models import (Product, Brand)
 
 class ProductListSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
+    # count = serializers.SerializerMethodField(method_name='get_review_count') # if I want to name it 'count'
+    review_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_review_count(self, object):
+        reviews = object.review_product.all().count()
+        return reviews
 
 
 class ProductDetailSerializer(serializers.ModelSerializer):
+    review_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_review_count(self, object):
+        reviews = object.review_product.all().count()
+        return reviews
 
 
 class BrandListSerializer(serializers.ModelSerializer):
