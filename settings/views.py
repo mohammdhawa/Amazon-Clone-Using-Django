a11125fdf073a798eb87e59102921ad2225eec48
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-07-27 15:11:33 +0300

    add data in home template

--- settings/views.py
+++ settings/views.py
@@ -1,7 +1,18 @@
+from django.db.models import Count
 from django.shortcuts import render
+from products.models import Product, Brand, Review
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'settings/home.html', {})
+    new_products = Product.objects.filter(flag='New')[:10]
+    sale_products = Product.objects.filter(flag='Sale')[:10]
+    feature_products = Product.objects.filter(flag='Feature')[:6]
+    brands = Brand.objects.annotate(product_count=Count('product_brand'))[:10]
+    reviews = Review.objects.all()[:6]
+
+    context = {'new_products': new_products, 'sale_products': sale_products,
+               'feature_products': feature_products, 'brands': brands,
+               'reviews': reviews}
+    return render(request, 'settings/home.html', context)
